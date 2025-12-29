8e45038946d86457e4d76f23d93296ad1bc6bf0d
BRSAMAyu <dengboren060427@163.com>
2025-12-30 00:05:31 +0800

    Use design tokens for task colors

--- mobile/lib/core/design/components/atoms/task_pill.dart
+++ mobile/lib/core/design/components/atoms/task_pill.dart
@@ -69,7 +69,7 @@ class TaskPill extends StatelessWidget {
       }
     } else {
       // Use task type-based colors
-      final taskColors = TaskColors(brightness: SparkleContext(context).sparkleColors.brightness);
+      final taskColors = TaskColors(colors: context.sparkleColors);
       background = taskColors.getTint(type);
       border = taskColors.getBorder(type);
       textColor = taskColors.getLabel(type);
